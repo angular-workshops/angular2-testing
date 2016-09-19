95bca6537ab0fafb813596f39daed61a2fd8bd06
Peter Bacon Darwin <pete@bacondarwin.com>
2016-09-19 12:01:50 +0100

    hero-search-service: add isolated test

--- tour-of-heroes/src/app/hero-search.service.ts
+++ tour-of-heroes/src/app/hero-search.service.ts
@@ -1,6 +1,7 @@
 import { Injectable }     from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
 
 import { Hero }           from './hero';
 
