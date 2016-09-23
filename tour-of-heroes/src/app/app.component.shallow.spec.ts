4190f678c35c68b5a23f0518e5840ddcd8f8009c
Peter Bacon Darwin <pete@bacondarwin.com>
2016-09-23 23:02:12 +0100

    heroes: simplify AppComponent shallow test

--- tour-of-heroes/src/app/app.component.shallow.spec.ts
+++ tour-of-heroes/src/app/app.component.shallow.spec.ts
@@ -4,25 +4,21 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent (shallow tests)', () => {
 
-  let fixture, component, element;
+  let fixture;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
       schemas: [NO_ERRORS_SCHEMA]
     });
     fixture = TestBed.createComponent(AppComponent);
-    component = fixture.debugElement.componentInstance;
-    element = fixture.debugElement.nativeElement;
   });
 
   it(`should have as title 'Tour of Heroes'`, async(() => {
-    expect(component.title).toEqual('Tour of Heroes');
+    expect(fixture.componentInstance.title).toEqual('Tour of Heroes');
   }));
 
   it('should render title in a h1 tag', async(() => {
     fixture.detectChanges();
-    expect(element.querySelector('h1').textContent).toContain('Tour of Heroes');
+    expect(fixture.nativeElement.querySelector('h1').textContent).toContain('Tour of Heroes');
   }));
 });
