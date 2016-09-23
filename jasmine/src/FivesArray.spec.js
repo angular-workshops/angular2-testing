3b004e934f33a839ac3a03fa3104f7f719647d4e
Peter Bacon Darwin <pete@bacondarwin.com>
2016-09-23 09:29:15 +0100

    jasmine: prepare FivesArray spec

--- jasmine/src/FivesArray.spec.js
+++ jasmine/src/FivesArray.spec.js
@@ -1,44 +1,16 @@
+describe('FivesArray', function() {
 
-// this will be written up as demonstration. This is the finished code
-describe("join", function() {
   var fives;
 
   beforeEach(function() {
     fives = new FivesArray();
   });
 
-  beforeEach(function () {
-    jasmine.addMatchers({
-      toBeEmptyArray: function () {
-        return {
-          compare: function (actual, expected) {
-            var array = actual;
+  describe("create", function() {
 
-            return {
-              pass: array.length === 0,
-              message: 'Expected [' + array + '] to have no elements'
-
-            };
-          }
-        };
-      }
-    });
+    xit('should be empty if 0 is passed', function() {
+      var a = fives.create(0);
+      expect(a.length).toBe(0);
+    })
   });
-
-  xit('should be empty if 0 is passed', function() {
-    var a = fives.create(0);
-    expect(a.length).toBe(0);
-  })
-
-  // with a custom matcher
-  xit('should be empty if 0 is passed [custom matcher]', function() {
-    var a = fives.create(0);
-    expect(a).toBeEmptyArray();
-  })
-
 });
-
-
-
-
-
