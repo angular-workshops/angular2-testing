c6b1fc1b55e21dc23983015e153286d9c2ddca79
Peter Bacon Darwin <pete@bacondarwin.com>
2016-09-23 21:30:45 +0100

    heroes: simplify HeroDetailComponent and spec

--- tour-of-heroes/src/app/hero-detail.component.isolated.spec.ts
+++ tour-of-heroes/src/app/hero-detail.component.isolated.spec.ts
@@ -2,58 +2,41 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { tick, fakeAsync } from '@angular/core/testing';
 
 describe('HeroDetailComponent (isolated tests)', () => {
-  let heroes = [
-    {id: 3, name: 'Magneta', strength: 4},
-    {id: 4, name: 'Dynama', strength: 2}
-  ];
+  const mockHero = {id: 3, name: 'Magneta', strength: 4};
+  let mockHeroService;
+  let mockRoute;
+  let component: HeroDetailComponent;
 
   beforeEach(() => {
-    this.mockHeroService = { getHero: () => {}, update: () => {} };
-    this.mockActivatedRoute = {params: []};
-    this.mockLocation = { back: () => {} };
+    mockHeroService = jasmine.createSpyObj('heroService', ['getHero', 'update']);
+    mockRoute = {params: []};
+    component = new HeroDetailComponent(mockHeroService, mockRoute);
   });
 
-  it('should grab the right hero on init', fakeAsync(() => {
-    this.mockActivatedRoute.params.push({id: '3'});
-    let retPromise = Promise.resolve(heroes[0]);
-    spyOn(this.mockHeroService, 'getHero').and.returnValue(retPromise);
-    const component = new HeroDetailComponent(this.mockHeroService, this.mockActivatedRoute, this.mockLocation);
+  describe('ngOnInit', () => {
+    it('should get the hero ', fakeAsync(() => {
+      mockRoute.params.push({id: '3'});
+      mockHeroService.getHero.and.returnValue(Promise.resolve(mockHero));
 
-    component.ngOnInit();
-    tick();
+      component.ngOnInit();
+      tick();
 
-    expect(component.hero).toEqual(heroes[0]);
-  }));
+      expect(mockHeroService.getHero).toHaveBeenCalledWith(3);
+      expect(component.hero).toEqual(mockHero);
+    }));
+  });
 
-  // this test mocks the window.history.back method. The next test assumes that it's been fixed.
-  // it('should call update on the right hero when save is called', fakeAsync(() => {
-  //   let retPromise = Promise.resolve();
-  //   spyOn(this.mockHeroService, 'update').and.returnValue(retPromise);
-  //   spyOn(window.history, 'back');
-  //   const component = new HeroDetailComponent(this.mockHeroService, this.mockActivatedRoute);
-  //   component.hero = heroes[1];
+  describe('save()', () => {
+    it('should update the heroService and then goBack', fakeAsync(() => {
+      mockHeroService.update.and.returnValue(Promise.resolve());
+      spyOn(component, 'goBack');
+      component.hero = mockHero;
 
-  //   component.save();
-  //   tick();
+      component.save();
+      tick();
 
-  //   expect(this.mockHeroService.update).toHaveBeenCalledWith(heroes[1]);
-  // }));
-
-  // this test is based on having the hero-detail component fixed with regards to 
-  // the use of the window global variable
-  it('should call update on the right hero when save is called', fakeAsync(() => {
-    let retPromise = Promise.resolve();
-    spyOn(this.mockHeroService, 'update').and.returnValue(retPromise);
-    spyOn(this.mockLocation, 'back');
-    const component = new HeroDetailComponent(this.mockHeroService, this.mockActivatedRoute, this.mockLocation);
-    component.hero = heroes[1];
-
-    component.save();
-    tick();
-
-    expect(this.mockHeroService.update).toHaveBeenCalledWith(heroes[1]);
-  }));
-
-
-
+      expect(mockHeroService.update).toHaveBeenCalledWith(mockHero);
+      expect(component.goBack).toHaveBeenCalled();
+    }));
+  });
 });
