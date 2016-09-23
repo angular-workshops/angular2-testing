e4bb55f7093f97708c95ff369dfa2d1aa09b4657
Peter Bacon Darwin <pete@bacondarwin.com>
2016-09-23 11:12:38 +0100

    jasmine: leave only skeleton Player spec

--- jasmine/src/Player.spec.js
+++ jasmine/src/Player.spec.js
@@ -10,37 +10,20 @@ describe("Player", function() {
   describe('play(song)', function() {
 
     it("should update currentlyPlayingSong and playing", function() {
-      player.play(song);
-      expect(player.currentlyPlayingSong).toEqual(song);
-      expect(player.playing).toBe(true);
     });
 
     // demonstrates custom matcher
     it('should play the song', function() {
-      player.play(song);
-      expect(player).toBePlaying(song);
     });
   });
 
   describe('makeFavorite()', function() {
     // demonstrates use of spies to intercept and test method calls
     it("should update the song that it is a favorite", function() {
-      spyOn(song, 'persistFavoriteStatus');
-
-      player.play(song);
-      player.makeFavorite();
-
-      expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
     });
 
     // same as above except with createSpyObj()
     it("should update the song that it is a favorite", function() {
-      var song = jasmine.createSpyObj('song', ['persistFavoriteStatus']);
-
-      player.play(song);
-      player.makeFavorite();
-
-      expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
     });
   });
 });
