567aa157e731cbe30146418d0588dd5c34e29635
Peter Bacon Darwin <pete@bacondarwin.com>
2016-09-24 06:18:48 +0100

    heroes: added alternative test styles

--- tour-of-heroes/src/app/hero.service.spec.ts
+++ tour-of-heroes/src/app/hero.service.spec.ts
@@ -1,42 +1,65 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async, fakeAsync, inject, tick } from '@angular/core/testing';
-import { MockBackend, MockConnection } from '@angular/http/testing';
+import { TestBed, fakeAsync, inject, tick } from '@angular/core/testing';
+import { MockBackend } from '@angular/http/testing';
+import { Http, BaseRequestOptions, Response, ResponseOptions } from '@angular/http';
 import { HeroService } from './hero.service';
-import { Http, ConnectionBackend, BaseRequestOptions, Response, ResponseOptions } from '@angular/http';
 
-describe('Service: HeroService', () => {
-  let heroes, matchingHero, connection;
+describe('HeroService', () => {
+  let mockResponse, matchingHero, connection;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         HeroService,
-        { provide: Http, useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
-          return new Http(backend, defaultOptions);
-        }, deps: [MockBackend, BaseRequestOptions]
-        },
         MockBackend,
-        BaseRequestOptions
+        BaseRequestOptions,
+        {
+          provide: Http,
+          useFactory: (backend, defaultOptions) => new Http(backend, defaultOptions),
+          deps: [MockBackend, BaseRequestOptions]
+        },
       ]
     });
 
-    heroes = [
+    const heroes = [
       {id: 2, name: 'Rubberman'},
       {id: 4, name: 'Dynama'}
     ];
+    mockResponse = new Response(new ResponseOptions({body: {data: heroes}, status: 200}));
   });
 
   describe('getHero', () => {
 
+    // Subscribing to the connection and storing it for later
     it('should return the correct hero when called with a valid id', fakeAsync(
-      inject([HeroService, MockBackend, Http], (service: HeroService, backend: MockBackend, http: Http) => {
-        backend.connections.subscribe(c => connection = c);
+      inject([HeroService, MockBackend], (service: HeroService, backend: MockBackend) => {
 
-        service.getHero(4).then(hero => {
-          matchingHero = hero;
-        });
-        connection.mockRespond(createResponse(heroes));
+        backend.connections.subscribe(c => connection = c);
+        service.getHero(4).then(hero => matchingHero = hero);
+        connection.mockRespond(mockResponse);
+        tick();
+
+        expect(matchingHero.id).toBe(4);
+        expect(matchingHero.name).toBe('Dynama');
+    })));
+
+    // Subscribing to the connection and responding inside the subscription
+    it('should return the correct hero when called with a valid id', fakeAsync(
+      inject([HeroService, MockBackend], (service: HeroService, backend: MockBackend) => {
+
+        backend.connections.subscribe(c => c.mockRespond(mockResponse));
+        service.getHero(4).then(hero => matchingHero = hero);
+        tick();
+
+        expect(matchingHero.id).toBe(4);
+        expect(matchingHero.name).toBe('Dynama');
+    })));
+
+    // Using the MockBackend.connectionsArray to get the connect
+    it('should return the correct hero when called with a valid id', fakeAsync(
+      inject([HeroService, MockBackend], (service: HeroService, backend: MockBackend) => {
+
+        service.getHero(4).then(hero => matchingHero = hero);
+        backend.connectionsArray[0].mockRespond(mockResponse);
         tick();
 
         expect(matchingHero.id).toBe(4);
@@ -44,21 +67,14 @@ describe('Service: HeroService', () => {
     })));
 
     it('should return an empty array when called with an invalid id', fakeAsync(
-      inject([HeroService, MockBackend, Http], (service: HeroService, backend: MockBackend, http: Http) => {
+      inject([HeroService, MockBackend], (service: HeroService, backend: MockBackend) => {
         backend.connections.subscribe(c => connection = c);
 
-        service.getHero(45).then(hero => {
-          matchingHero = hero;
-        });
-        connection.mockRespond(createResponse(heroes));
+        service.getHero(45).then(hero => matchingHero = hero);
+        connection.mockRespond(mockResponse);
         tick();
 
         expect(matchingHero).toBeUndefined();
     })));
-
   });
-
-  function createResponse(data) {
-    return new Response(new ResponseOptions({body: {data: data}, status: 200}));
-  }
 });
