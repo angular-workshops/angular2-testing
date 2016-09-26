d97b02f62ce4a606952556f464c7c1b0ad2d8567
Michael Giambalvo <heathkit@gmail.com>
2016-09-26 11:13:37 +0100

    Use a different sauce account, only use a single browser.
    There seems to be an issue with 'beforeLaunch' and multicapabilities that breaks ts-node when
    launching.

--- tour-of-heroes/protractor-saucelabs.conf.js
+++ tour-of-heroes/protractor-saucelabs.conf.js
@@ -1,39 +1,11 @@
-var defaultConfig = require('./protractor.conf.js').config;
-
 var testName = 'Tour of Heroes Smoke Tests'
+var SpecReporter = require('jasmine-spec-reporter');
 
-var ciConfig = {
-  // Note that seleniumAddress must be unset in order for SauceLabs to work.
-  seleniumAddress: false,
+exports.config = {
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
 
-  directConnect: false,
-  multiCapabilities: [{
-    'browserName': 'chrome',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': testName,
-    'version': '51',
-    'selenium-version': '2.53.1',
-    'chromedriver-version': '2.22',
-    'platform': 'OS X 10.9'
-  }, {
-    'browserName': 'firefox',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': testName,
-    'version': '44',
-    'selenium-version': '2.53.1'
-  }, {
-    'browserName': 'safari',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': testName,
-    'version': '9',
-    'selenium-version': '2.44.0' // Use an old version because Safari has
-                                 // issues loading pages after 2.44.
-  }, {
+  capabilities: {
     'browserName': 'internet explorer',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER,
@@ -41,8 +13,26 @@ var ciConfig = {
     'version': '11',
     'selenium-version': '2.53.1',
     'platform': 'Windows 7'
-  }]
-};
+  },
 
-exports.config = Object.assign(defaultConfig, ciConfig);
-console.log(exports.config);
+  allScriptsTimeout: 30000,
+  specs: [
+    './e2e/**/*.e2e-spec.ts'
+  ],
+  baseUrl: 'http://localhost:4200/',
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    print: function() {}
+  },
+  useAllAngular2AppRoots: true,
+  beforeLaunch: function() {
+    require('ts-node').register({
+      project: 'e2e'
+    });
+  },
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(new SpecReporter());
+  }
+};
