8cac732b854cf21d985f276ebe71700c012bfd3e
Michael Giambalvo <heathkit@gmail.com>
2016-09-26 01:49:04 +0100

    Remember to update webdriver.

--- scripts/travis_setup.sh
+++ scripts/travis_setup.sh
@@ -3,6 +3,9 @@
 set -ex -o pipefail
 cd `dirname $0`
 
+cd ../tour-of-heroes
+webdriver-manager update
+
 # Start Xvfb when running locally.
 if [[ ${TRAVIS} && (${CI_MODE} == "local_e2e") ]]; then
   sh -e /etc/init.d/xvfb start
