1ceb7ae53a981e7c6bf439dcbffdbe73f7c8dd44
Michael Giambalvo <heathkit@gmail.com>
2016-09-26 01:29:15 +0100

    Don't run the unit tests when doing saucelabs.

--- scripts/travis_setup.sh
+++ scripts/travis_setup.sh
@@ -4,6 +4,6 @@ set -ex -o pipefail
 cd `dirname $0`
 
 # Start Xvfb when running locally.
-if [[ ${TRAVIS} && (${CI_MODE} == "local") ]]; then
+if [[ ${TRAVIS} && (${CI_MODE} == "local_e2e") ]]; then
   sh -e /etc/init.d/xvfb start
 fi
--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -5,12 +5,16 @@ cd `dirname $0`
 cd ../tour-of-heroes
 
 ng build
-npm run lint
-npm run test -- --watch=false
 
-# When using saucelabs, use a different config.
-if [[ ${TRAVIS} && (${CI_MODE} == "saucelabs") ]]; then
-  PROTRACTOR_CONFIG="protractor-saucelabs.conf.js"
+if [[ ${TRAVIS} && (${CI_MODE} == "local_e2e") ]]; then
+  npm run lint
+  npm run test -- --watch=false
+  PROTRACTOR_CONFIG="./protractor.conf.js"
 fi
 
-concurrently --success first --kill-others \"lite-server\" \"protractor $PROTRACTOR_CONFIG\"
+# When using saucelabs, use a different config.
+if [[ ${TRAVIS} && (${CI_MODE} == "saucelabs_e2e") ]]; then
+  PROTRACTOR_CONFIG="./protractor-saucelabs.conf.js"
+fi
+
+concurrently --success first --kill-others "lite-server" "protractor $PROTRACTOR_CONFIG"
